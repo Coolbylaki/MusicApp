0f8678e473cc5b228887611b4600d8eb8a81b7b5
Lazar Stojanović <lakisd98@gmail.com>
2023-01-30 16:06:58 +0100

    Add: validation for registration form

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -8,6 +8,11 @@ import {
   required,
   min,
   max,
+  min_value as minValue,
+  max_value as maxValue,
+  email,
+  confirmed,
+  not_one_of,
   alpha_spaces as alphaSpaces,
 } from "@vee-validate/rules";
 
@@ -21,6 +26,11 @@ export default {
     defineRule("min", min);
     defineRule("max", max);
     defineRule("alpha_spaces", alphaSpaces);
+    defineRule("email", email);
+    defineRule("min_value", minValue);
+    defineRule("max_value", maxValue);
+    defineRule("confirmed", confirmed);
+    defineRule("excluded", not_one_of);
   },
 };
 // It can accept options as second argument also, which allow other developers to configure it.
