e7bce26d70e604338964a4d95d0ebe67c8b72d3b
Lazar Stojanović <lakisd98@gmail.com>
2023-01-31 15:52:28 +0100

    Add: login form login method

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,4 +1,11 @@
 <template>
+  <div
+    class="text-white text-center font-bold p-4 rounded mb-4"
+    v-if="login_show_alert"
+    :class="login_alert_variant"
+  >
+    {{ login_alert_msg }}
+  </div>
   <vee-form @submit="login" :validation-schema="loginSchema">
     <!-- Email -->
     <div class="mb-3">
@@ -25,6 +32,7 @@
     <button
       type="submit"
       class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+      :disabled="login_in_submission"
     >
       Submit
     </button>
@@ -42,10 +50,21 @@ export default {
         email: "required|email",
         password: "required|min:9|max:100",
       },
+      login_in_submission: false,
+      login_show_alert: false,
+      login_alert_variant: "bg-blue-500",
+      login_alert_msg: "Please wait! We are logging you in.",
     };
   },
   methods: {
     login(values) {
+      this.login_in_submission = true;
+      this.login_show_alert = true;
+      this.login_alert_variant = "bg-blue-500";
+      this.login_alert_msg = "Please wait! We are logging you in.";
+
+      this.login_alert_variant = "bg-green-500";
+      this.login_alert_msg = "Success! You are now logged in.";
       console.log(values);
     },
   },
