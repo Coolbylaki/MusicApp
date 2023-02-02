94c5f8907708dc8bd524fc35dbf4b0a7595eb989
Lazar Stojanović <lakisd98@gmail.com>
2023-02-02 16:27:05 +0100

    Add: user store register action function

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,8 +1,24 @@
 import { defineStore } from "pinia";
+import { auth, usersCollection } from "@/includes/firebase";
 
 // First param is ID of the store
 export default defineStore("user", {
   state: () => ({
     userLoggedIn: false,
   }),
+  actions: {
+    // Use actions when it changes the store/state
+    async register(values) {
+      await auth.createUserWithEmailAndPassword(values.email, values.password);
+
+      await usersCollection.add({
+        name: values.name,
+        email: values.email,
+        country: values.country,
+      });
+
+      // Update state
+      this.userLoggedIn = true;
+    },
+  },
 });
