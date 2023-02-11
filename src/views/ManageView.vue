94c417fd1a362ff7c99aa01264fa8ad3447d1d62
Lazar Stojanović <lakisd98@gmail.com>
2023-02-11 04:05:24 +0100

    Update: app functionality to cancel uploads

--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -2,7 +2,7 @@
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div class="col-span-1">
-        <app-upload />
+        <app-upload ref="upload" />
       </div>
       <div class="col-span-2">
         <div
@@ -146,5 +146,9 @@ export default {
   //   }
   // },
   components: { AppUpload },
+  // beforeRouteLeave(to, from, next) {
+  //   this.$refs.upload.cancelUploads();
+  //   next();
+  // },
 };
 </script>
