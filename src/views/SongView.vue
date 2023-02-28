941d00f21e80dacdae0f87fa810d19d3c88f2858
Lazar Stojanović <lakisd98@gmail.com>
2023-02-28 23:15:34 +0100

    Update: comment component to display correct number of comments

--- src/views/SongView.vue
+++ src/views/SongView.vue
@@ -25,9 +25,7 @@
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title"
-          >Comments ({{ this.sortedComments.length }})</span
-        >
+        <span class="card-title">Comments ({{ song.comment_count }})</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -102,6 +100,9 @@ export default {
       return;
     }
 
+    const { sort } = this.$route.query;
+    this.sort = sort === "1" || sort === "2" ? sort : "1";
+
     this.song = docSnapshot.data();
     this.getComments();
   },
@@ -121,6 +122,11 @@ export default {
       };
 
       await commentsCollection.add(comment);
+
+      this.song.comment_count += 1;
+      await songsCollection.doc(this.$route.params.id).update({
+        comment_count: this.song.comment_count,
+      });
 
       this.getComments();
 
@@ -142,6 +148,18 @@ export default {
           docID: doc.id,
           ...doc.data(),
         });
+      });
+    },
+  },
+  watch: {
+    sort(newVal) {
+      if (newVal === this.$route.query.sort) {
+        return;
+      }
+      this.$router.push({
+        query: {
+          sort: newVal,
+        },
       });
     },
   },
