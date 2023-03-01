ee25960eeae6597f329df575a3e52cf05cb5740a
Lazar Stojanović <lakisd98@gmail.com>
2023-03-01 22:57:02 +0100

    Update: icon directive custom modifiers

--- src/directives/icon.js
+++ src/directives/icon.js
@@ -1,7 +1,23 @@
 export default {
-  beforeMount(el) {
+  beforeMount(el, binding) {
+    let iconClass = `fa fa-${binding.value} text-xl`;
+
+    if (binding.arg === "full") {
+      iconClass = binding.value;
+    }
+
+    if (binding.modifiers.right) {
+      iconClass += " float-right";
+    }
+
+    if (binding.modifiers.yellow) {
+      iconClass += " text-yellow-400";
+    } else {
+      iconClass += " text-green-400";
+    }
+
     el.innerHTML += `<i
-    class="fa fa-headphones-alt float-right text-green-400 text-xl"
+    class="${iconClass}"
   ></i>`;
   },
 };
