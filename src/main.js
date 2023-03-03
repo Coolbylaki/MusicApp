105140f0626b7f298ef8f7b202883066fac9a0d5
Lazar Stojanović <lakisd98@gmail.com>
2023-03-04 00:15:28 +0100

    Add: site progress bar

--- src/main.js
+++ src/main.js
@@ -8,13 +8,17 @@ import { auth } from "./includes/firebase";
 import Icon from "./directives/icon";
 import i18n from "./includes/i18n";
 import { registerSW } from "virtual:pwa-register";
-import GlobalComponents from "./includes/_globals";
+import GlobalComponents from "@/includes/_globals";
+import progressBar from "@/includes/progressBar";
 
 import "./assets/base.css";
 import "./assets/main.css";
+import "nprogress/nprogress.css";
 
 // Workbox
 registerSW({ immediate: true });
+
+progressBar(router);
 
 let app;
 auth.onAuthStateChanged(() => {
