18fee46c8ed5674ceb73fa0e2df07bcb5d263d40
Lazar Stojanović <lakisd98@gmail.com>
2023-03-03 02:34:24 +0100

    Add: pwa manifest config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,31 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Music App",
+        short_name: "vue-music",
+        theme_color: "#ff5e3a",
+        icons: [
+          {
+            src: "assets/img/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
